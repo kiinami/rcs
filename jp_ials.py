d2a4185d643afb4fdb73728a4c328524b4e2fc1d
kiinami <kinami@posteo.net>
2023-12-18 11:57:10 +0100

    ials optuna

---
+++ jp_ials.py
@@ -0,0 +1,42 @@
+import os
+from utils import *
+import optuna
+from Recommenders.MatrixFactorization.IALSRecommender import IALSRecommender
+
+
+data, usermap, itemmap, users = load_data2()
+data_train, data_test, data_val = split_data2(data, 0, 0.2)
+
+
+study_name = "IALS"
+study = optuna.create_study(
+    study_name=study_name,
+    storage=get_database_url(),
+    load_if_exists=True,
+    direction="maximize",
+)
+
+
+def objective(trial):
+    num_factors = trial.suggest_int('num_factors', 1, 500)
+    epochs = trial.suggest_categorical('epochs', list(range(1, 501, 50)))
+    confidence_scaling = trial.suggest_categorical('confidence_scaling', ["linear", "log"])
+    alpha = trial.suggest_float('alpha', 1e-7, 100, log=True)
+    epsilon = trial.suggest_float('epsilon', 1e-7, 100, log=True)
+    reg = trial.suggest_float('reg', 1e-5, 1, log=True)
+    
+    recommender = IALSRecommender(data_train, verbose=False)
+    recommender.fit(
+        epochs=epochs,
+        num_factors=num_factors,
+        confidence_scaling=confidence_scaling,
+        alpha=alpha,
+        epsilon=epsilon,
+        reg=reg,
+    )
+    _, _, ev_map, _, _ = evaluator(recommender, data_train, data_val)
+    
+    return ev_map
+
+
+study.optimize(objective, n_trials=150)
