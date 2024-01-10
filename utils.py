1da72c9427d6fcebb8ed06e6820f201e70c1561b
frax01 <francescomartignoni1@gmail.com>
2024-01-10 16:19:28 +0100

    win file

--- utils.py
+++ utils.py
@@ -285,3 +285,53 @@ def submission2(recommender, users, usermap, itemmap, data_train):
                 item_list = toppoprecommender.recommend(0, cutoff=10, remove_seen_flag=True)
                 item_list = [itemmap[i] for i in item_list]
                 writer.writerow([usermap[user_id], ' '.join(map(str, item_list))])
+
+
+from Recommenders.BaseRecommender import BaseRecommender
+import scipy.sparse as sps
+from numpy import linalg as LA
+class DifferentLossScoresHybridRecommender(BaseRecommender):
+    """ ScoresHybridRecommender
+    Hybrid of two prediction scores R = R1/norm*alpha + R2/norm*(1-alpha) where R1 and R2 come from
+    algorithms trained on different loss functions.
+
+    """
+
+    RECOMMENDER_NAME = "DifferentLossScoresHybridRecommender"
+
+
+    def __init__(self, URM_train, recommender_1, recommender_2):
+        super(DifferentLossScoresHybridRecommender, self).__init__(URM_train)
+
+        self.URM_train = sps.csr_matrix(URM_train)
+        self.recommender_1 = recommender_1
+        self.recommender_2 = recommender_2
+        
+        
+        
+    def fit(self, norm, alpha = 0.5):
+
+        self.alpha = alpha
+        self.norm = norm
+
+
+    def _compute_item_score(self, user_id_array, items_to_compute):
+        
+        item_weights_1 = self.recommender_1._compute_item_score(user_id_array)
+        item_weights_2 = self.recommender_2._compute_item_score(user_id_array)
+
+        norm_item_weights_1 = LA.norm(item_weights_1, self.norm)
+        norm_item_weights_2 = LA.norm(item_weights_2, self.norm)
+        
+        
+        if norm_item_weights_1 == 0:
+            norm_item_weights_1 = 0.0000001
+            #raise ValueError("Norm {} of item weights for recommender 1 is zero. Avoiding division by zero".format(self.norm))
+        
+        if norm_item_weights_2 == 0:
+            norm_item_weights_2 = 0.0000001
+            #raise ValueError("Norm {} of item weights for recommender 2 is zero. Avoiding division by zero".format(self.norm))
+        
+        item_weights = item_weights_1 / norm_item_weights_1 * self.alpha + item_weights_2 / norm_item_weights_2 * (1-self.alpha)
+
+        return item_weights
